7b4a87e431c6e1ba9b5d868c13dcea68e1faa81e
GideonBature <infoaboutgideon@gmail.com>
2024-05-21 00:33:15 +0100

    add review controller

---
+++ src/routes/reviewRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const reviewController = require('../controllers/reviewController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.post('/reviews', authMiddleware, reviewController.createReview);
+router.get('/reviews', authMiddleware, reviewController.getAllReviews);
+router.get('/reviews/:id', authMiddleware, reviewController.getReviewById);
+
+module.exports = router
