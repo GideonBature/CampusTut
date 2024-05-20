44b84563d69d6481679dc4adf97edcf30bc3dd5e
GideonBature <infoaboutgideon@gmail.com>
2024-05-21 00:22:41 +0100

    add user route

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.get('/users', authMiddleware, userController.getAllUsers);
+router.get('/users/:id', authMiddleware, userController.getUserById);
+router.put('/users/:id', authMiddleware, userController.updateUser);
+
+module.exports = router;
