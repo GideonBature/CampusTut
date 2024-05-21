4997f87f5b62343560b4dfb68cfbf9da0a0efa9f
GideonBature <infoaboutgideon@gmail.com>
2024-05-21 19:41:37 +0100

    fix code

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -22,12 +22,20 @@ exports.login = async (req, res) => {
 
     try {
         const user = await User.findOne({ email });
+
         if (!user) {
             return res.status(400).json({ message: "Invalid email or password" });
         }
 
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid email or password" });
+        }
+
         const token = generateToken(user);
-        await setAsync(user._id, token);
+
+        await setAsync(token, user._id.toString());
         res.json({ token });
     } catch (error) {
         res.status(500).json({ message: error.message });
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,13 +1,11 @@
 const jwt = require("jsonwebtoken");
 const { jwt_secret } = require("../config/config");
 
-const generateToken = (payload) => {
-    return jwt.sign({ id: user._id, type: user.type }, jwt_secret, { expiresIn: "1d",
-    });
+const generateToken = (user) => {
+    const { id, type } = user;
+    return jwt.sign({ id, type }, jwt_secret, { expiresIn: "1d" });
 };
 
-const verifyToken = (token) => {
-    return jwt.verify(token, jwt_secret);
-};
+const verifyToken = (token) => jwt.verify(token, jwt_secret);
 
 module.exports = { generateToken, verifyToken };
